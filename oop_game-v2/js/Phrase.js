9a79de90ad750995bcf79eff9faffaa2dfa62c57
anneebrown <61443022+anneebrown@users.noreply.github.com>
2020-04-18 22:14:56 -0700

    regex functional

--- oop_game-v2/js/Phrase.js
+++ oop_game-v2/js/Phrase.js
@@ -35,9 +35,40 @@ class Phrase {
                 li.className = `hide letter ${letter}`;
             }
         }
-
     };
+
+/**
+* Checks if passed letter is in phrase
+* @param (string) letter - Letter to check
+*/
+//I got the basic idea for the regex here: https://stackoverflow.com/questions/7053506/how-to-put-variable-in-regular-expression-match
+    checkLetter(letter) {
+        let tested = letter; 
+        let regex = new RegExp(tested, 'g');
+        if (this.phrase.match(regex)){
+            return true; 
+        }  else {
+            return false;
+        }
+    };
+
+/**
+* Displays passed letter on screen after a match is found
+* @param (string) letter - Letter to display
+*/
+    showMatchedLetter(letter) {
+        let match = document.getElementsByClassName(`hide letter ${letter}`);
+        match.className = `show`;
+    };
+
 }
+
+
+// var string = 'asdgghjjkhkh'; = this.phrase
+// var string2 = letter;
+// var regex = new RegExp( string2, 'g' );
+// string.match(regex);
+
 
 
 
