e8ee546d9a043ed07586d537756790d99beb4245
anneebrown <61443022+anneebrown@users.noreply.github.com>
2020-04-20 18:39:25 -0700

    css changed

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # project 4
  
+Changes to the CSS: 
+- the background color on start is a brighter blue
+- the color for wrong buttons is red
+- the color for correctly guessed buttons is green
+- the background color on win is yellow
--- oop_game-v2/js/app.js
+++ oop_game-v2/js/app.js
@@ -10,18 +10,14 @@ document.getElementById('btn__reset').addEventListener('click', (event) => {
 
 //gets an array of buttons 
 let keyButtons = document.getElementById('qwerty');
-//keyButtonsSliced = keyButtons.slice(0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25);
-//console.log(keyButtons);
 
-// let aButton = keyButtons.filter(keyButton => keyButton.innerText = 'a');
-// console.log(aButton);
-
-//for (let i= 0; i < keyButtons.length; i+= 1){
 keyButtons.addEventListener('click', (event) => {
-    //console.log(event.target.textContent);
     if (event.target.tagName === 'BUTTON') {
     game.handleInteraction(event.target);
     }
 });
-//};
 
+
+// document.addEventListener('keyup', keyButtons, (event) => {
+//     game.handleInteraction(event.target);
+// });
